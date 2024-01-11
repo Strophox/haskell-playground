9dd291a5da2d70329e07f653f79f6a77621b8f1b
Strophox <strophox@gmail.com>
2024-01-11 04:06:59 +0100

    minor reformat

--- brainfu/brainfu.hs
+++ brainfu/brainfu.hs
@@ -14,19 +14,18 @@ run code input = go ([],code) ([],repeat 0) input where
   go :: (String,String) -> ([Int],[Int]) -> String -> String
   go (_,[]) _ _ = "# program finished.\n"
   go (cl,c:cr) (ml,m:mr) i = case c of
-    '<' -> go (c:cl,cr) (tail ml,head ml:m:mr) i
-    '>' -> go (c:cl,cr) (m:ml,mr) i
-    '+' -> go (c:cl,cr) (ml,(succ m`mod`256):mr) i
-    '-' -> go (c:cl,cr) (ml,(pred m`mod`256):mr) i
+    '<' -> go (c:cl,cr) (tail ml, head ml:m:mr) i
+    '>' -> go (c:cl,cr) (m:ml, mr) i
+    '+' -> go (c:cl,cr) (ml, (succ m`mod`256):mr) i
+    '-' -> go (c:cl,cr) (ml, (pred m`mod`256):mr) i
     '.' -> toEnum m : go (c:cl,cr) (ml,m:mr) i
     ',' | null i -> "# input ended.\n"
-    ',' -> go (c:cl,cr) (ml,fromEnum (head i):mr) (tail i)
-    '[' | m==0
-        -> let (l,r) = goto (c:cl,cr) 1 in go (l,r) (ml,m:mr) i
-    ']' | m/=0
-        -> let (r,l) = goto (c:cr,cl) (-1) in go (l,r) (ml,m:mr) i
+    ',' -> go (c:cl,cr) (ml, fromEnum (head i):mr) (tail i)
+    '[' | m==0 -> let (l,r) = findMatch (c:cl,cr) 1
+        in go (l,r) (ml,m:mr) i
+    ']' | m/=0 -> let (r,l) = findMatch (c:cr,cl) (-1)
+        in go (l,r) (ml,m:mr) i
     _   -> go (c:cl,cr) (ml,m:mr) i
 
-  goto :: (String,String) -> Int -> (String,String)
-  goto (l,c:r) n = if n == 0 then (l,c:r)
-                   else goto (c:l,r) (case c of '[' -> n+1 ; ']' -> n-1 ; _ -> n)
+  findMatch :: (String,String) -> Int -> (String,String)
+  findMatch (l,c:r) n = if n == 0 then (l,c:r) else findMatch (c:l,r) (case c of '[' -> n+1 ; ']' -> n-1 ; _ -> n)
