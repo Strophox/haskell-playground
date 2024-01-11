17c815b9f4756bdd1db9112e16ada7c20d2e1bed
Strophox <strophox@gmail.com>
2024-01-11 03:24:00 +0100

    bf interpreter and test programs

---
+++ brainfu/brainfu.hs
@@ -0,0 +1,32 @@
+import System.Environment (getProgName,getArgs)
+import System.Exit (exitFailure,exitSuccess)
+
+main = do
+  progName <- getProgName
+  args <- getArgs
+  code <- case args of
+    [arg] -> readFile arg
+    _     -> putStrLn ("Usage: "<>progName<>" [file]") >> exitFailure
+  interact (run code) --putStr (run code "test124")
+
+run :: String -> String -> String
+run code input = go ([],code) ([],repeat 0) input where
+  go :: (String,String) -> ([Int],[Int]) -> String -> String
+  go (_,[]) _ _ = "# program finished.\n"
+  go (cl,c:cr) (ml,m:mr) i = case c of
+    '<' -> go (c:cl,cr) (tail ml,head ml:m:mr) i
+    '>' -> go (c:cl,cr) (m:ml,mr) i
+    '+' -> go (c:cl,cr) (ml,(succ m`mod`256):mr) i
+    '-' -> go (c:cl,cr) (ml,(pred m`mod`256):mr) i
+    '.' -> toEnum m : go (c:cl,cr) (ml,m:mr) i
+    ',' | null i -> "# input ended.\n"
+    ',' -> go (c:cl,cr) (ml,fromEnum (head i):mr) (tail i)
+    '[' | m==0
+        -> let (l,r) = goto (c:cl,cr) 1 in go (l,r) (ml,m:mr) i
+    ']' | m/=0
+        -> let (r,l) = goto (c:cr,cl) (-1) in go (l,r) (ml,m:mr) i
+    _   -> go (c:cl,cr) (ml,m:mr) i
+
+  goto :: (String,String) -> Int -> (String,String)
+  goto (l,c:r) n = if n == 0 then (l,c:r)
+                   else goto (c:l,r) (case c of '[' -> n+1 ; ']' -> n-1 ; _ -> n)
